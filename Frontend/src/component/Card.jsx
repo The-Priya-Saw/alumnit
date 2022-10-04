7af59578ba617566ce0f4f8ea765aef97b0f8b3a
priya-saw-1999 <priyasaw245@gmail.com>
2022-10-04 04:13:15 -0700

    edited homepage

--- Frontend/src/component/Card.jsx
+++ Frontend/src/component/Card.jsx
@@ -1,12 +1,46 @@
 import "./Card.css";
 const Card = (props) => {
   return (
-    <div className="Card">
-      <img className="img" src={props.img} />
-      <span className="facultyName">{props.facultyName}</span>
-      <p className="jobTitle">{props.jobTitle}</p>
+    // <div className="Card">
+    //   <img className="img" src={props.img} />
+    //   <span className="facultyName">{props.facultyName}</span>
+    //   <p className="jobTitle">{props.jobTitle}</p>
+    // </div>
+    <div>
+      <div class="card1">
+        <img src={props.img} alt="none"/>
+        <div class="descriptions">
+          <h1>Event</h1>
+          <p> hello world </p>
+          <button>
+            <i class=""></i>
+            <a href="#">View more </a>
+          </button>
+        </div>
+      </div>
+      <div class="youtubeBtn">
+{/* 
+        <a target="_blank" href="">
+          <span>View more</span>
+          <i class="fab fa-youtube"></i>
+        </a> */}
+
+      </div>
     </div>
+
+
+
+
+
+
+
+
+
+
   );
 };
 
 export default Card;
+
+
+
