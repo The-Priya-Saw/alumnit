a1d1d804f7dc42739d821f76102d907febb52158
priya saw <priyasaw2452gmail.com>
2022-10-07 17:29:37 +0530

    created dropdown menu for Navbar

--- Frontend/src/component/Navbar.jsx
+++ Frontend/src/component/Navbar.jsx
@@ -1,26 +1,71 @@
 import bvp_logo from "../images/bvp_logo.png";
 import "./Navbar.css";
-const Navbar = (props) => {
-return(
-    <nav class="navbar">
-     <div className="brandLogo">
-      <img className="bvp_logo" src={bvp_logo}/>
-     </div>
-     <div className={"links " + (props.isTransperent ?  "transperent" : "non-transperent")} >
-      <a href="/">Home</a>
-      {
-        props.isAdmin ? <a href="/admin/inviteAlumni">Invite Alumni</a> : null
-      }
-      <a href="/community">Community</a>
-      <a href="/event">Events</a>
-      <a href="/career">Career</a>
-      {
-        props.isLogin ? <a href="/">Log out</a> : <a href="login">Login</a>
-      }
-      {/* <a href="login">{props.isLogin ? "Logout" : "Login"}</a> */}
+import Dropdown from 'react-bootstrap/Dropdown';
 
-     </div>
-   </nav>
-); 
+
+
+// const locations = [
+//   {
+//     label: 'New York',
+//     value: 'newYork',
+//   },
+//   {
+//     label: 'Oslo',
+//     value: 'oslo',
+//   },
+//   {
+//     label: 'Istanbul',
+//     value: 'istanbul',
+//   }
+// ];
+
+
+const Navbar = (props) => {
+  return (
+    <nav class="navbar">
+      <div className="brandLogo">
+        <img className="bvp_logo" src={bvp_logo} />
+      </div>
+      <div className={"links " + (props.isTransperent ? "transperent" : "non-transperent")} >
+        <a href="/">Home</a>
+        {
+          props.isAdmin ? <a href="/admin/inviteAlumni">Invite Alumni</a> : null
+        }
+        <a href="/community">Community</a>
+        <a href="/event">Events</a>
+        <a href="/career">Career</a>
+
+
+
+        {
+          props.isLogin ? <a href="/">Log out</a> : <a href="login">Login</a>
+        }
+
+
+        {/* <a href="login">{props.isLogin ? "Logout" : "Login"}</a> */}
+
+        {/* <Dropdown
+          name="location"
+          title="Select location"
+          list={locations}
+
+        /> */}
+
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+           Admin
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Invite Alumni</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">View Users</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Logout</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+
+      </div>
+    </nav>
+  );
 }
 export default Navbar;
